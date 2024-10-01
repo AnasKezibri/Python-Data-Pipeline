f2cc11092d28a1931dba5615d9ef837990c35b14
HKezibri <anas.kezibri@mel.lincoln.fr>
2024-10-01 10:41:11 +0200

    Use unix docker url

--- drugs-publiction-graph/airflow/dags/kedro_drugs_pipeline_dag.py
+++ drugs-publiction-graph/airflow/dags/kedro_drugs_pipeline_dag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
 
-# Définir les arguments de base du DAG
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -11,21 +11,22 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-# Initialiser le DAG
+
 dag = DAG(
     'kedro_drugs_pipeline_dag',
     default_args=default_args,
-    description='DAG pour exécuter le data pipeline du graphe de liason entre medicaments et publications',
+    description='DAG to run Kedro drugs pipeline in Docker',
     schedule_interval=timedelta(minutes=20),
     catchup=False,
 )
 
-# Définir l'opérateur Docker pour exécuter Kedro
 run_kedro_pipeline = DockerOperator(
     task_id='run_drugs_pipeline',
     image='kedro-drugs-pipeline:latest',
     container_name='kedro_container',
     auto_remove=True,
     command='kedro run',
+    docker_url='unix://var/run/docker.sock',
+    network_mode='bridge',
     dag=dag,
 )
